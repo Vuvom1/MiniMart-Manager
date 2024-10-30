d4940a22046aa813fe9f228883150cc70141248a
Vuvom1 <21522808@gm.uit.edu.vn>
2024-10-30 15:24:17 +0700

    update collapsed row table

--- client/minimart-manager/src/components/Table/CollapsedRowTable.tsx
+++ client/minimart-manager/src/components/Table/CollapsedRowTable.tsx
@@ -89,6 +89,10 @@ const CollapsedRowTable: React.FC<ItemsProps> = ({
 
     };
 
+    const handleCopyId = (id: string) => {
+        navigator.clipboard.writeText(id);
+    };
+
     return (
         <div className="flex flex-col bg-white shadow-md rounded-lg p-4 h-full">
             <div className='flex mb-4 items-center justify-between'>
@@ -146,12 +150,19 @@ const CollapsedRowTable: React.FC<ItemsProps> = ({
                                 {dataFields.slice(0, showData).map((field, fieldIndex) => (
                                     <td key={fieldIndex} className="px-4 py-4 border-b border-gray-200">
                                         {field === "_id" ? (
-                                            <span className="relative group">
-                                                {item[field].slice(0, 8) + '...'}
-                                                <span className="left-0 bottom-full mb-2 hidden group-hover:block bg-gray-700 text-white text-xs px-2 py-1 rounded shadow-lg">
-                                                    {item[field]}
-                                                </span>
-                                            </span>
+                                            <span className="relative group flex gap-x-2 items-center">
+                                            <button
+                                                className="ml-2 p-1 border w-6 h-6 rounded hover:bg-gray-300"
+                                                onClick={() => handleCopyId(item[field])}
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                                                </svg>
+
+                                            </button>
+                                            {item[field].slice(0, 8) + '...'}
+
+                                        </span>
                                         ) : 
                                         (field === "status" ? (
                                             
